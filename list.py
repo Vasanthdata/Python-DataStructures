8a7cbe16fdf0e88aa72892cbcef689e557a5882f
Vasanth <vasanth634@gmail.com>
2024-12-11 14:10:12 +0530

    linked list

---
+++ list.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    # Insert at the beginning
+    def insert_at_beginning(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+    
+    # Insert at the end
+    def insert_at_end(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+        temp.next = new_node
+
+    # Traverse and print the linked list
+    def traverse(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end="--> ")
+            temp = temp.next
+        print("None")
+    
+    

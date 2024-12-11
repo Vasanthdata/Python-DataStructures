5e576216c91e66369b0095221b699009c71e0ec1
Vasanth <vasanth634@gmail.com>
2024-12-11 21:04:44 +0530

    List updated

--- list.py
+++ list.py
@@ -12,7 +12,7 @@ class LinkedList:
         new_node = Node(data)
         new_node.next = self.head
         self.head = new_node
-    
+
     # Insert at the end
     def insert_at_end(self, data):
         new_node = Node(data)
@@ -28,8 +28,40 @@ class LinkedList:
     def traverse(self):
         temp = self.head
         while temp:
-            print(temp.data, end="--> ")
+            print(temp.data, end=" -> ")
             temp = temp.next
         print("None")
-    
-    
+
+    # Delete a node
+    def delete_node(self, key):
+        temp = self.head
+
+        # If head node itself holds the key
+        if temp and temp.data == key:
+            self.head = temp.next
+            temp = None
+            return
+
+        # Search for the key
+        prev = None
+        while temp and temp.data != key:
+            prev = temp
+            temp = temp.next
+
+        # Key not found
+        if not temp:
+            return
+
+        # Unlink the node
+        prev.next = temp.next
+        temp = None
+
+# Example Usage
+ll = LinkedList()
+ll.insert_at_beginning(1)
+# ll.insert_at_beginning(2)
+# ll.insert_at_end(4)
+# ll.insert_at_end(5)
+ll.traverse()  # Output: 2 -> 3 -> 4 -> 5 -> None
+# ll.delete_node(3)
+# ll.traverse()  # Output: 2 -> 4 -> 5 -> None
